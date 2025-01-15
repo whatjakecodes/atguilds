05954e5d385b4aeef9fb520d81bfa097256c93fe
Jake Stout <kyjstout@gmail.com>
2025-01-15 11:04:41 -0500

    Fix unused css warnings

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -58,16 +58,6 @@
         --background: rgba(255, 255, 255, 0.7);
     }
 
-    svg {
-        width: 2em;
-        height: 3em;
-        display: block;
-    }
-
-    path {
-        fill: var(--background);
-    }
-
     ul {
         position: relative;
         padding: 0;
