f0814e834130eaa111926e610b548c356f9fe24f
Jake Stout <kyjstout@gmail.com>
2025-01-15 11:14:15 -0500

    Move logic out of prerendering

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,9 +4,9 @@ import { DB_PATH } from '$env/static/private';
 
 const db = createDb(DB_PATH);
 await migrateToLatest(db);
-const client = await createClient(db);
 
 export async function handle({ event, resolve }) {
+	const client = await createClient(db);
 	const sessionId = event.cookies.get('sid') || null;
 	console.log(`found sessionID: ${sessionId} (${event.request.method} ${event.url})`);
 	event.locals.session = sessionId ? { did: sessionId } : undefined;
