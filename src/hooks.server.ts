d2be71d378db7b63d55706fc76e6fea911ae35a5
Jake Stout <kyjstout@gmail.com>
2025-01-15 12:53:28 -0500

    Skip oauth client setup for pre-rendered pages at buildtime

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,15 +1,20 @@
 import { createClient } from '$lib/server/oauthClient.server';
 import { createDb, migrateToLatest } from '$lib/server/db';
 import { DB_PATH } from '$env/static/private';
+import { building } from '$app/environment';
 
 const db = createDb(DB_PATH);
 await migrateToLatest(db);
 
 export async function handle({ event, resolve }) {
-	const client = await createClient(db);
-	const sessionId = event.cookies.get('sid') || null;
-	console.log(`found sessionID: ${sessionId} (${event.request.method} ${event.url})`);
-	event.locals.session = sessionId ? { did: sessionId } : undefined;
-	event.locals.client = client;
+	if (!building) {
+		// skip oauth client setup during build-time pre-rendering
+		const client = await createClient(db!);
+		const sessionId = event.cookies.get('sid') || null;
+		console.log(`found sessionID: ${sessionId} (${event.request.method} ${event.url})`);
+		event.locals.session = sessionId ? { did: sessionId } : undefined;
+		event.locals.client = client;
+	}
+
 	return await resolve(event);
 }
