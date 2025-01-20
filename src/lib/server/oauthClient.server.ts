006f9baa0b9c2cdf5d836d9424e6cf83112c0962
Jake Stout <kyjstout@gmail.com>
2025-01-20 17:27:34 -0500

    Debug metadata issue in vercel

--- src/lib/server/oauthClient.server.ts
+++ src/lib/server/oauthClient.server.ts
@@ -17,12 +17,16 @@ export const createClient = async (db: Database) => {
 		console.log(`Using VERCEL_URL for oauth redirect: ${publicUrl}`);
 	}
 	const url = publicUrl || `http://127.0.0.1:${PORT}`;
+	const clientId = publicUrl
+		? `${url}/client-metadata.json`
+		: `http://localhost?redirect_uri=${enc(`${url}/oauth/callback`)}&scope=${enc('atproto transition:generic')}`;
+
+	console.log(`OAuth Client ID: ${clientId}`);
+
 	return new NodeOAuthClient({
 		clientMetadata: {
 			client_name: 'ATGuilds SvelteKit App',
-			client_id: publicUrl
-				? `${url}/client-metadata.json`
-				: `http://localhost?redirect_uri=${enc(`${url}/oauth/callback`)}&scope=${enc('atproto transition:generic')}`,
+			client_id: clientId,
 			client_uri: url,
 			redirect_uris: [`${url}/oauth/callback`],
 			scope: 'atproto transition:generic',
--- src/routes/client-metadata.json/+server.ts
+++ src/routes/client-metadata.json/+server.ts
@@ -2,6 +2,7 @@ import type { RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async ({ locals }) => {
 	const clientMetadata = locals.client.clientMetadata;
+	console.log({clientMetadata});
 	return new Response(JSON.stringify(clientMetadata), {
 		headers: {
 			'content-type': 'application/json'
